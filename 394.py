83cdaff36faa5d06377c8a4e2545e507613357cd
Connie <c.amer@icloud.com>
2024-06-21 13:16:08 +0100

    decode string stack

---
+++ 394.py
@@ -0,0 +1,24 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+
+        for i in range(len(s)):
+            string = ""
+
+            if s[i] != "]":
+                stack.append(s[i])
+            else:
+                while stack[-1] != "[":
+                    string = stack.pop() + string
+                stack.pop()
+
+                n = stack.pop()
+
+                for count in range(0, int(n)):
+                    stack.append(string)
+
+
+        return "".join(stack)
+                    
+
+                
