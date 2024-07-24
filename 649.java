456c778f5678a15ba97ef0a7c2296a7ddd87e963
Connie <c.amer@icloud.com>
2024-07-24 11:57:00 +0100

    649

---
+++ 649.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> rad = new LinkedList<>();
+        Queue<Integer> dir = new LinkedList<>();
+
+        int n = senate.length();
+
+        for (int i = 0; i < n; i++){
+            if (senate.charAt(i) == 'R'){
+                rad.add(i);
+            } else {
+                dir.add(i);
+            }
+        }
+
+        while (!rad.isEmpty() && !dir.isEmpty()) {
+            if (rad.peek() < dir.peek()) {
+                rad.add(n++);
+            } else {
+                dir.add(n++);
+            }
+
+            dir.poll();
+            rad.poll();
+        } 
+
+        return (rad.isEmpty() ? ("Dire"):("Radiant"));
+    }
+}
